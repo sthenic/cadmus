2f3fef328bd6b51f5b488dedea927ff1e8498b64
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-01-13 18:04:41 +0100

    Update Cadmus __main__ file to direct the user to the two separate tools in doc/ and fig/.

--- cadmus/__main__.py
+++ cadmus/__main__.py
@@ -1,1 +1,5 @@
-print('Cadmus main function called!')
+print('Cadmus\n')
+print('cadmus.doc - Generate reStrucuturedText output from decorated TeX\n'
+      '             source files.')
+print('cadmus.fig - Generate figures from code snippets in combination with\n'
+      '             template files.')
