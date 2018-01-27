bac09dd9ddf7b56cd418bd4bbe6c46d2ac14264b
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-01-27 21:46:27 +0100

    Format default values of control sequence options as literals.
    Add PATCH version to setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Cadmus',
-      version='0.1',
+      version='0.1.0',
       description='A LaTeX front-end to Sphinx',
       author='Marcus Eriksson',
       author_email='marcus.jr.eriksson@gmail.com',
