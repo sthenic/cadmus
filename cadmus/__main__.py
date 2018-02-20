669e9aa4b076cc90ac4ea3b461a4114b04985962
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-02-20 23:26:18 +0100

    Add argparse to Cadmus main file.
    Add version query.

--- cadmus/__main__.py
+++ cadmus/__main__.py
@@ -1,5 +1,22 @@
-print('Cadmus\n')
-print('cadmus.doc - Generate reStrucuturedText output from decorated TeX\n'
-      '             source files.')
-print('cadmus.fig - Generate figures from code snippets in combination with\n'
-      '             template files.')
+import argparse
+
+VERSION_ = '0.1.0'
+
+parser = argparse.ArgumentParser(
+    description = 'Cadmus v' + VERSION_ + '\n\n'
+    'cadmus.doc - Generate reStrucuturedText output from decorated LaTeX\n'
+    '             source files.\n'
+    'cadmus.fig - Generate figures from code snippets in combination with\n'
+    '             template files.\n',
+    formatter_class = argparse.RawTextHelpFormatter
+)
+
+parser.add_argument('-v', '--version',
+                    help='Print version and exit.',
+                    action='store_true')
+
+# Parse input arguments
+args = parser.parse_args()
+
+if args.version:
+    print(VERSION_)
