bb566017392bf71ee570e9bcdf7f4c5dc36b8837
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-03-25 17:08:01 +0200

    docgen: Refactor control sequence RST formatting code.

--- cadmus/doc/control_sequence.py
+++ cadmus/doc/control_sequence.py
@@ -26,70 +26,73 @@ class ControlSequence(DObject):
         self._args.append({'name': arg, 'descr': descr})
         return
 
-    def format_opts(self, rst_conf):
+    def format_opt_table(self):
         if not self._opts:
             return ''
-        # Add label
-        formatted_output = '.. _' + self._name + '_opts:\n\n'
-        # Add header
-        formatted_output += \
-            'Options\n' + rst_conf['cs_subsection_char']*7 + '\n'
         # Constuct table dict
         header = ['Option', 'Description', 'Default']
         body = []
         for opt in self._opts:
-            body.append([
-                '``' + opt['name'] + '``',
-                opt['descr'],
-                '``' + opt['default'] + '``'
-            ])
-        table_dict = {'header': header, 'body': body}
-        # Get formatted output
-        formatted_output += format_table(table_dict)
+            body.append(['``' + opt['name'] + '``', opt['descr'],
+                         '``' + opt['default'] + '``'])
 
-        return formatted_output + '\n'
+        return format_table({'header': header, 'body': body})
 
-    def format_keyword_arguments(self, rst_conf):
+    def format_opts(self, rst_conf):
+        if not self._opts:
+            return ''
+        formatted_output = (
+            # Add label
+            '.. _' + self._name + '_opts:\n\n'
+            # Add header
+            + 'Options\n' + rst_conf['cs_subsection_char']*7 + '\n'
+        )
+
+        return formatted_output + self.format_opt_table() + '\n'
+
+    def format_kwarg_table(self):
         if not self._kwargs:
             return ''
-        # Add label
-        formatted_output = '.. _' + self._name + '_kwargs:\n\n'
-        # Add header
-        formatted_output += \
-            'Keyword Arguments\n' + rst_conf['cs_subsection_char']*17 + '\n'
         # Constuct table dict
         header = ['Keyword', 'Description', 'Default']
         body = []
         for kwarg in self._kwargs:
-            body.append([
-                '``' + kwarg['name'] + '``',
-                kwarg['descr'],
-                '``' + kwarg['default'] + '``'
-            ])
-        table_dict = {'header': header, 'body': body}
-        # Get formatted output
-        formatted_output += format_table(table_dict)
+            body.append(['``' + kwarg['name'] + '``', kwarg['descr'],
+                         '``' + kwarg['default'] + '``'])
 
-        return formatted_output + '\n'
+        return format_table({'header': header, 'body': body})
 
-    def format_args(self, rst_conf):
+    def format_keyword_arguments(self, rst_conf):
+        if not self._kwargs:
+            return ''
+        formatted_output = (
+            # Add label
+            '.. _' + self._name + '_kwargs:\n\n'
+            # Add header
+            + 'Keyword Arguments\n' + rst_conf['cs_subsection_char']*17 + '\n'
+        )
+
+        return formatted_output + self.format_kwarg_table() + '\n'
+
+    def format_arg_table(self):
         if not self._args:
             return ''
-        # Add label
-        formatted_output = '.. _' + self._name + '_args:\n\n'
-        # Add header
-        formatted_output += \
-            'Arguments\n' + rst_conf['cs_subsection_char']*9 + '\n'
         # Constuct table dict
         header = ['Argument', 'Description']
         body = []
         for arg in self._args:
-            body.append([
-                '``' + arg['name'] + '``',
-                arg['descr']
-            ])
-        table_dict = {'header': header, 'body': body}
-        # Get formatted output
-        formatted_output += format_table(table_dict)
+            body.append(['``' + arg['name'] + '``', arg['descr']])
 
-        return formatted_output + '\n'
+        return format_table({'header': header, 'body': body})
+
+    def format_args(self, rst_conf):
+        if not self._args:
+            return ''
+        formatted_output = (
+            # Add label
+            '.. _' + self._name + '_args:\n\n'
+            # Add header
+            + 'Arguments\n' + rst_conf['cs_subsection_char']*9 + '\n'
+        )
+
+        return formatted_output + self.format_arg_table() + '\n'
